81491fe77d90d52a4fa0be68d638d790c8c1f3e4
Tom Yates <me@tpy.io>
2023-04-02 13:58:04 +0100

    updated logic + readme

--- Invoke-ValheimServer.ps1
+++ Invoke-ValheimServer.ps1
@@ -9,23 +9,25 @@ param (
 $ConfigFile = "D:\Repos\ValheimControl\config.json"
 $Config = Get-Content $ConfigFile | ConvertFrom-Json
 
+$LogFile = $(join-path ($config.forceinstalldir) ($config.logfile))
+
 Function Start-Valheim {
         $env:SteamAppId="892970"
-        $valargs = "-nographics -batchmode -name `"$($config.servername)`" -port $($config.port) -world `"$($config.world)`" -password `"$($config.password)`" -logfile `"$(join-path ($config.forceinstalldir) ($config.logfile))`""
+        $valargs = "-nographics -batchmode -name `"$($config.servername)`" -port $($config.port) -world `"$($config.world)`" -password `"$($config.password)`" -logfile `"$($LogFile)`""
         Start-Process "$($config.forceinstalldir)\valheim_server.exe" -ArgumentList $valargs
 }
 
 Function Stop-Valheim($ProcessID) {
-    #Sends Ctrl+C to the Valheim window, which saves the server first and shuts down cleanly
+        #Sends Ctrl+C to the Valheim window, which saves the server first and shuts down cleanly
         $encodedCommand = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes("Add-Type -Names 'w' -Name 'k' -M '[DllImport(""kernel32.dll"")]public static extern bool FreeConsole();[DllImport(""kernel32.dll"")]public static extern bool AttachConsole(uint p);[DllImport(""kernel32.dll"")]public static extern bool SetConsoleCtrlHandler(uint h, bool a);[DllImport(""kernel32.dll"")]public static extern bool GenerateConsoleCtrlEvent(uint e, uint p);public static void SendCtrlC(uint p){FreeConsole();AttachConsole(p);GenerateConsoleCtrlEvent(0, 0);}';[w.k]::SendCtrlC($ProcessID)"))
-        start-process powershell.exe -argument "-nologo -noprofile -executionpolicy bypass -EncodedCommand $encodedCommand"
+        start-process powershell.exe -argument "-nologo -noprofile -executionpolicy bypass -EncodedCommand $encodedCommand" -NoNewWindow
         write-host "Waiting for Process $($ProcessID) to stop." -NoNewline
         Do {
             Write-host "." -NoNewline            
             Start-Sleep -Seconds 1
         }
-        while (Get-Process -ID (Get-ValheimStatus).Id)
-        Write-Output "PID '$($ProcessID)' stopped."        
+        while (get-process valheim_server -ErrorAction SilentlyContinue)
+        Write-Output "`nPID '$($ProcessID)' stopped."        
 }
 
 Function Get-ValheimStatus{
@@ -36,6 +38,7 @@ Function Get-ValheimStatus{
     }else{
         $RunningStatus= $False
     }
+    
     $Results = @{"Active"=$RunningStatus;"ID"=$ID}
     Return $Results
 }   
@@ -48,16 +51,34 @@ Switch ($Action){
         }
     }
     "Stop" {
-        Write-Output "Stopping..."
-        If($($ValheimStatus.Active)){
+        If($(Get-ValheimStatus).Active){
+            Write-Output "Stopping..."
             Stop-Valheim $(Get-ValheimStatus).ID
+        }else{
+            Write-Output "Not Started - doing nothing."
         }
     }
     "Update" {
-        Write-Output "Update $Action"
-    }
-    "Status" {
-        Write-Output "Status $Action"
+        $ValheimWasRunning = $false
+        If($(Get-ValheimStatus).Active){
+            Write-Output "Stopping server..."
+            Stop-Valheim $(Get-ValheimStatus).ID
+            $ValheimWasRunning = $true
+        }
+        Write-Output "Updating server..."
+        Start-Process "$($config.steamcmd)" -ArgumentList "+force_install_dir `"$($config.forceinstalldir)`" +login anonymous +app_update $($config.gameid) validate +exit" -wait
+        If ($ValheimWasRunning){
+            Start-Valheim
+        }
     }
 
+    "Status" {
+        $Status = Get-ValheimStatus
+        If($Status.Active -eq $true){
+            Write-Output "Valheim Server running on Process ID '$($Status.ID)'"
+        }
+        else{
+            Write-Output "Valheim Server is not running."
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# ValheimControl
+# Invoke-ValheimServer
+
+## Summary
+A script to manage the running of a Valheim Dedicated Server. 
+Stores some settings in config.json.
+Requires [SteamCMD](https://developer.valvesoftware.com/wiki/SteamCMD#Downloading_SteamCMD) for the updating process. 
+This was written so it could be controlled via [PowerShell Universal](https://www.ironmansoftware.com/powershell-universal/), so my wife could start\stop the server with ease in my absence. 
+
+## Examples
+### Start the Valheim Dedicated Server
+```PowerShell
+PS> Invoke-ValheimServer.ps1 -Action Start
+```
+
+### Stop the Valheim Dedicated Server
+```PowerShell
+PS> Invoke-ValheimServer.ps1 -Action Stop
+```
+
+### Get Status of the Valheim Dedicated Server
+```PowerShell
+PS> Invoke-ValheimServer.ps1 -Action Status
+```
+
+### Update the Valheim Dedicated Server
+
+```PowerShell
+PS> Invoke-ValheimServer.ps1 -Action Start
+```
+Note: If the server is running before updating, it will get restarted automatically. If the server is started, it will be gracefully shutdown. 
+
+## Config File
+Update this config file with desired settings. 
+```json
+{
+    "servername":  "myservername",
+    "world": "Dedicated",
+    "port": "2456",
+    "password": "asecretpasswordplease",
+    "gameid":  "896660",
+    "steamcmd":  "C:\\Steamcmd\\steamcmd.exe",
+    "forceinstalldir":  "C:\\valheim-server",
+    "logfile": "server.log"
+}
+```
